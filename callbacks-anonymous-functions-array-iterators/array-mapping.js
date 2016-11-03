0d72a8a6816cd94ad8d4aa960fc93b4a73ab34a7
Josh Coles <josh@joshcoles.com>
2016-11-03 20:25:41 +0000

    mapped array to return true with comparison

--- callbacks-anonymous-functions-array-iterators/array-mapping.js
+++ callbacks-anonymous-functions-array-iterators/array-mapping.js
@@ -1,1 +1,15 @@
-d
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(function(dataSet) {
+  var total = ((dataSet.x * dataSet.x) + (dataSet.y * dataSet.y));
+  var z = Math.sqrt(total);
+  return z;
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
